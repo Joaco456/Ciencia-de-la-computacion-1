2d026643055bbfcf9fdd16c0195f712a6ccedba7
Joaquín Maúrtua/CCOMP-1 <joaquin.maurtua@ucsp.edu.pe>
2017-06-07 23:22:35 -0500

    Add files via upload

--- JuegoFinal/include/Nave.h
+++ JuegoFinal/include/Nave.h
@@ -6,12 +6,18 @@ class Nave
 {
     private:
         int x,y;
+        int corazones;
+        int vidas;
 
     public:
-        Nave(int ,int);
+        Nave(int ,int, int, int);
+        void coordenada(int,int);
+        void ocultar();
         void crear();
         void borrar();
         void mover();
+        void VidaCorazones();
+        void Morir();
 };
 
 #endif // NAVE_H
--- JuegoFinal/main.cpp
+++ JuegoFinal/main.cpp
@@ -4,21 +4,17 @@
 #include <conio.h>
 #include "Nave.h"
 
-#define ARRIBA 72
-#define IZQUIERDA 75
-#define DERECHA 77
-#define ABAJO 80
+
 
 
 using namespace std;
 
-void gotoxy(int x, int y){
+void coordenada(int x, int y){
     HANDLE identi;
     identi = GetStdHandle(STD_OUTPUT_HANDLE);
     COORD pos_cursor;
     pos_cursor.X = x;
     pos_cursor.Y = y;
-
     SetConsoleCursorPosition(identi,pos_cursor);
 }
 void Ocultar_cursor(){
@@ -26,21 +22,41 @@ void Ocultar_cursor(){
     identi = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE_CURSOR_INFO cursor;
     cursor.dwSize = 50;
-    cursor.bVisible = FALSE;
+    cursor.bVisible = false;
+    SetConsoleCursorInfo(identi, &cursor);
+}
 
-    SetConsoleCursorInfo(identi,&cursor);
+void Limites(){
+    for(int i = 2; i<78; i++){ //limites arriba y abajo
+        coordenada(i,3); printf("%c",205);
+        coordenada(i,33); printf("%c",205);
+    }
+
+    for(int i =4; i<33; i++){ //limites derecha y izquierda
+        coordenada(2,i); printf("%c",186);
+        coordenada(77,i); printf("%c",186);
+    }
+    //bordes
+    coordenada(2,3); printf("%c",201);
+    coordenada(2,33); printf("%c",200);
+    coordenada(77,3); printf("%c",187);
+    coordenada(77,33); printf("%c",188);
+
 }
 
 int main()
 {
-    Ocultar_cursor();
-    Nave nave(5,5);
+    Nave nave(7,7,3,3);
     nave.crear();
-
+    nave.borrar();
+    nave.ocultar();
     bool GameOver = false;
     while(!GameOver){
 
     nave.mover();
+    nave.VidaCorazones();
+    nave.Morir();
+    Limites();
     Sleep(30);
     }
 
